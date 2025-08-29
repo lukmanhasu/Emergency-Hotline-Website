e4200a30f07ac13c745011ad6feedfb62e2cd6f1
lukmanhasu <contact.lukmanhasu@gmail.com>
2025-08-29 19:28:22 +0600

    added update file

--- script.js
+++ script.js
@@ -10,18 +10,7 @@ heartBtns.forEach((heartBtns, index) => {
     });
 });
 
-// // alert function
-// let Buttons = document.querySelectorAll('.call-btn');
-// Buttons.forEach(call => {
-//     call.addEventListener('click', function () {
-//         let card = call.closest('.card');
-//         let serviceName = card.querySelector(".card-title").textContent;
-//         let ServiceNumber = card.querySelector('.call-number').textContent;
-//         alert("Service:" + serviceName + "\nNumber:" + ServiceNumber);
-//     })
-// });
-
-//Reduce Coin function
+// alert function
 let defultCoins = 100;
 let coinCounter = document.getElementById('coin');
 coinCounter.textContent = defultCoins;
@@ -41,19 +30,38 @@ callBtn.forEach(CoinBtn => {
         let ServiceNumber = card.querySelector('.call-number').textContent;
         alert("Service:" + serviceName + "\nNumber:" + ServiceNumber);
 
+        let historyTitle = document.getElementById('history-titlle')
+        let historyNumb = document.getElementById('history-number')
+
+        historyTitle.textContent = serviceName;
+        historyNumb.textContent = ServiceNumber;
+        
+        let time = new Date();
+        let timeString = time.getHours().toString().padStart(2, '0') + ":" +
+            time.getMinutes().toString().padStart(2, '0') + ":" +
+            time.getSeconds().toString().padStart(2, '0');
+        historyTime.textContent = timeString;
+
     })
 })
 
+//copy on click number
+let copyNumber = document.querySelectorAll('.copy-btn');
+let totalCopies = 2; // Total copies counter
+let headCopyBtn = document.getElementById('head-copy-btn');
 
-// if (defultCoins >= 20) {
-//     defultCoins -= 20;
-//     coinCounter.textContent = defultCoins;
-// }
-// else {
-//     alert('Not enough coins for calling')
-//     return
-// }
-// let card = coinBtn.closest('.card');
-// let serviceName = card.querySelector(".card-title").textContent;
-// let ServiceNumber = card.querySelector('.call-number').textContent;
-// alert("Service:" + serviceName + "\nNumber:" + ServiceNumber);
+copyNumber.forEach(number => {
+    number.addEventListener('click', function () {
+        let card = number.closest('.card');
+        if (!card) return;
+
+        let ServiceNumber = card.querySelector('.call-number').textContent;
+
+        navigator.clipboard.writeText(ServiceNumber)
+            .then(() => {
+                totalCopies++;
+                headCopyBtn.textContent = totalCopies;
+                alert('Copied to clipboard: ' + ServiceNumber);
+            })
+    });
+});
